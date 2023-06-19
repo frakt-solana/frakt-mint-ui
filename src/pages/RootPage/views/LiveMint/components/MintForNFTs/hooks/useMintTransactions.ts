4694ecec8d28fedaa1f09b43bfbf79165fd265af
iamsphere <ezekiel.9218@gmail.com>
2023-06-19 10:52:22 +0300

    clear selection after bulk mint

--- src/pages/RootPage/views/LiveMint/components/MintForNFTs/hooks/useMintTransactions.ts
+++ src/pages/RootPage/views/LiveMint/components/MintForNFTs/hooks/useMintTransactions.ts
@@ -118,6 +118,7 @@ export const useMintTransactions = ({ selection, hideNFT, clearSelection }) => {
     } catch (error) {
       console.log(error)
     } finally {
+      clearSelection()
       closeLoadingModal()
     }
   }
