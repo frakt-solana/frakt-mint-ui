3a93844c5d964ebf0fbc65c84cf75c36a2b6e09c
iamsphere <ezekiel.9218@gmail.com>
2023-06-19 20:33:14 +0300

    change rpc

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,4 @@ export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 export const ENDPOINT = IS_DEVELOPMENT
   ? process.env.DEVELOPMENT_RPC_ENDPOINT
-  : process.env.RPC_HELIOS;
+  : process.env.RPC_ENDPOINT;
